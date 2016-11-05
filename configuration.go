21279936b18e9d57ba8d84a044baa2f8b79b4b49
Fabio Falci <fabiofalci@gmail.com>
2016-11-05 23:04:15 +0000

    Polishing

---
+++ configuration.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"strings"
+	"io/ioutil"
+	"github.com/smallfish/simpleyaml"
+	"fmt"
+	"strconv"
+)
+
+type Configuration struct {
+	GlobalHeaders map[string]bool
+	GlobalUrl string
+	Endpoints map[string]*Endpoint
+	Requests map[string]*Request
+}
+
+func NewConfiguration() *Configuration {
+	configuration := &Configuration{
+		GlobalHeaders: 	make(map[string]bool),
+		Endpoints: make(map[string]*Endpoint),
+		Requests: make(map[string]*Request),
+	}
+	return configuration
+}
+
+func (conf *Configuration) LoadAll(file string) {
+	if !strings.HasSuffix(file, ".yaml") {
+		file = file + ".yaml"
+	}
+	conf.readConfiguration(file)
+}
+
+func (conf *Configuration) readConfiguration(moduleDefinition string) {
+	source, err := ioutil.ReadFile(moduleDefinition)
+	if err != nil {
+		panic(err)
+	}
+
+	yaml, err := simpleyaml.NewYaml(source)
+	if err != nil {
+		panic(err)
+	}
+	asMap, err := yaml.Map()
+	if err != nil {
+		panic(err)
+	}
+
+	for key := range asMap {
+		if conf.isConfiguration(key.(string)) {
+			conf.addConfiguration(key.(string), yaml)
+		}
+	}
+
+	conf.readEndpoints(asMap["endpoints"].(map[interface{}]interface{}), yaml)
+	conf.readRequests(asMap["requests"].(map[interface{}]interface{}))
+}
+
+func (conf *Configuration) ShowAll() {
+	for name := range conf.Requests {
+		request := conf.Requests[name]
+		request.show()
+		fmt.Println("")
+	}
+}
+
+func (conf *Configuration) ShowRequest(requestName string) {
+	request := conf.Requests[requestName]
+	if request != nil {
+		request.show()
+		return
+	}
+
+	endpoint := conf.Endpoints[requestName]
+	if endpoint != nil {
+		endpoint.show()
+	}
+}
+
+func (conf *Configuration) readRequests(requestsMap map[interface{}]interface{}) {
+	for key := range requestsMap {
+		keyAsString := key.(string)
+		conf.addRequest(keyAsString, requestsMap[key])
+	}
+}
+
+func (conf *Configuration) readEndpoints(endpointMap map[interface{}]interface{}, yaml *simpleyaml.Yaml) {
+	for key := range endpointMap {
+		keyAsString := key.(string)
+		conf.addEndpoint(keyAsString, yaml)
+	}
+}
+
+func (conf *Configuration) addRequest(name string, value interface{}) {
+	request := &Request{Name: name, Headers: make(map[string]bool)}
+	conf.Requests[name] = request
+
+	request.Parameters = value.(map[interface{}]interface{})
+
+	endpointName := request.Parameters["endpoint"]
+	endpoint := conf.Endpoints[endpointName.(string)]
+	request.Method = endpoint.Method
+	request.Url = endpoint.Url
+	request.Path = endpoint.Path
+	request.Query = endpoint.Query
+
+	for k,v := range endpoint.Headers {
+		request.Headers[k] = v
+	}
+
+	for k := range request.Parameters {
+		toReplace := "{"+k.(string)+"}"
+		replacement := conf.getReplacement(request.Parameters[k])
+		request.Url = strings.Replace(request.Url, toReplace, replacement, -1)
+		request.Path = strings.Replace(request.Path, toReplace, replacement, -1)
+		request.Query = strings.Replace(request.Query, toReplace, replacement, -1)
+
+		for header := range request.Headers {
+			replaced := strings.Replace(header, toReplace, replacement, -1)
+			if (header != replaced) {
+				request.Headers[replaced] = true
+				delete(request.Headers, header)
+			}
+		}
+	}
+}
+
+
+func (conf *Configuration) getReplacement(value interface{}) string {
+	switch value.(type) {
+	default:
+		return "<<Error: invalid tpe>>"
+	case bool:
+		return strconv.FormatBool(value.(bool))
+	case int:
+		return strconv.Itoa(value.(int))
+	case string:
+		return value.(string)
+	}
+}
+
+
+func (conf *Configuration) addEndpoint(name string, yaml *simpleyaml.Yaml) {
+	endpoint := &Endpoint{Name: name, Headers: make(map[string]bool)};
+	conf.Endpoints[name] = endpoint
+
+	path, err := yaml.GetPath("endpoints", name, "path").String()
+	if err != nil {
+		panic(err)
+	}
+	endpoint.Path = path
+
+	query, err := yaml.GetPath("endpoints", name, "query").String()
+	if err == nil {
+		endpoint.Query = query
+	}
+
+	url, err := yaml.GetPath("endpoints", name, "url").String()
+	if err == nil {
+		endpoint.Url = url
+	} else {
+		endpoint.Url = conf.GlobalUrl
+	}
+
+	method, err := yaml.GetPath("endpoints", name, "method").String()
+	if err == nil {
+		endpoint.Method = method
+	} else {
+		endpoint.Method = "GET"
+	}
+
+	data, err := yaml.GetPath("endpoints", name, "data").Bool()
+	if err == nil {
+		endpoint.HasData = data
+	}
+
+	headers, err := yaml.GetPath("endpoints", name, "headers").Array()
+	if err == nil {
+		for i := range headers {
+			endpoint.Headers[headers[i].(string)] = true
+		}
+	}
+
+	for globalHeader := range conf.GlobalHeaders {
+		endpoint.Headers[globalHeader] = true
+	}
+}
+
+func (conf *Configuration) addConfiguration(name string, yaml *simpleyaml.Yaml) {
+	if name == "headers" {
+		headers, _ := yaml.Get(name).Array()
+		for i := range headers {
+			conf.GlobalHeaders[headers[i].(string)] = true
+		}
+	} else if name == "url" {
+		conf.GlobalUrl, _ = yaml.Get(name).String()
+	}
+}
+
+func (conf *Configuration) isConfiguration(name string) bool {
+	return name == "headers" || name == "url"
+}
+
--- main.go
+++ main.go
@@ -2,25 +2,15 @@ package main
 
 import (
 	"os"
-	"io/ioutil"
 	"text/template"
 	"strings"
-	"strconv"
 	"fmt"
 
-	"github.com/smallfish/simpleyaml"
 	"github.com/urfave/cli"
 	"bytes"
 	"regexp"
 	"bufio"
 	"os/exec"
-)
-
-var (
-	globalHeaders map[string]bool = make(map[string]bool)
-	globalUrl string
-	endpoints map[string]*Endpoint = make(map[string]*Endpoint)
-	requests map[string]*Request = make(map[string]*Request)
 )
 
 const showCurlTemplate = `curl {{.Url}}{{.Path}}{{if .Query}}?{{.Query}}{{end}} \
@@ -64,7 +54,13 @@ type Request struct {
 	Headers map[string]bool
 }
 
+type Executor struct {
+	Conf *Configuration
+}
+
 func main() {
+	conf := NewConfiguration()
+	executor := &Executor{Conf: conf}
 	app := cli.NewApp()
 	app.Version = "0.1.0"
 	var file string
@@ -81,24 +77,24 @@ func main() {
 		{
 			Name: "show-all",
 			Action: func(c *cli.Context) error {
-				loadAll(file)
-				showAll()
+				conf.LoadAll(file)
+				conf.ShowAll()
 				return nil
 			},
 		},
 		{
 			Name: "show",
 			Action: func(c *cli.Context) error {
-				loadAll(file)
-				showRequest(c.Args().First())
+				conf.LoadAll(file)
+				conf.ShowRequest(c.Args().First())
 				return nil
 			},
 		},
 		{
 			Name: "run",
 			Action: func(c *cli.Context) error {
-				loadAll(file)
-				runRequest(c.Args().First())
+				conf.LoadAll(file)
+				executor.RunRequest(c.Args().First())
 				return nil
 			},
 		},
@@ -107,177 +103,19 @@ func main() {
 	app.Run(os.Args)
 }
 
-func runRequest(requestName string) {
-	request := requests[requestName]
+func (executor *Executor) RunRequest(requestName string) {
+	request := executor.Conf.Requests[requestName]
 	if request != nil {
 		request.run()
 	}
 
-	endpoint := endpoints[requestName]
+	endpoint := executor.Conf.Endpoints[requestName]
 	if endpoint != nil {
 		endpoint.run()
 	}
 }
 
-func showRequest(requestName string) {
-	request := requests[requestName]
-	if request != nil {
-		request.show()
-		return
-	}
 
-	endpoint := endpoints[requestName]
-	if endpoint != nil {
-		endpoint.show()
-	}
-}
-
-func loadAll(file string) {
-	if !strings.HasSuffix(file, ".yaml") {
-		file = file + ".yaml"
-	}
-	readConfiguration(file)
-}
-
-func readConfiguration(moduleDefinition string) {
-	source, err := ioutil.ReadFile(moduleDefinition)
-	if err != nil {
-		panic(err)
-	}
-
-	yaml, err := simpleyaml.NewYaml(source)
-	if err != nil {
-		panic(err)
-	}
-	asMap, err := yaml.Map()
-	if err != nil {
-		panic(err)
-	}
-
-	for key := range asMap {
-		if isConfiguration(key.(string)) {
-			addConfiguration(key.(string), yaml)
-		}
-	}
-
-	readEndpoints(asMap["endpoints"].(map[interface{}]interface{}), yaml)
-	readRequests(asMap["requests"].(map[interface{}]interface{}))
-}
-
-func showAll() {
-	for name := range requests {
-		request := requests[name]
-		request.show()
-		fmt.Println("")
-	}
-}
-func readRequests(requestsMap map[interface{}]interface{}) {
-	for key := range requestsMap {
-		keyAsString := key.(string)
-		addRequest(keyAsString, requestsMap[key])
-	}
-}
-
-func readEndpoints(endpointMap map[interface{}]interface{}, yaml *simpleyaml.Yaml) {
-	for key := range endpointMap {
-		keyAsString := key.(string)
-		addEndpoint(keyAsString, yaml)
-	}
-}
-
-func addRequest(name string, value interface{}) {
-	request := &Request{Name: name, Headers: make(map[string]bool)}
-	requests[name] = request
-
-	request.Parameters = value.(map[interface{}]interface{})
-
-	endpointName := request.Parameters["endpoint"]
-	endpoint := endpoints[endpointName.(string)]
-	request.Method = endpoint.Method
-	request.Url = endpoint.Url
-	request.Path = endpoint.Path
-	request.Query = endpoint.Query
-
-	for k,v := range endpoint.Headers {
-		request.Headers[k] = v
-	}
-
-	for k := range request.Parameters {
-		toReplace := "{"+k.(string)+"}"
-		replacement := getReplacement(request.Parameters[k])
-		request.Url = strings.Replace(request.Url, toReplace, replacement, -1)
-		request.Path = strings.Replace(request.Path, toReplace, replacement, -1)
-		request.Query = strings.Replace(request.Query, toReplace, replacement, -1)
-
-		for header := range request.Headers {
-			replaced := strings.Replace(header, toReplace, replacement, -1)
-			if (header != replaced) {
-				request.Headers[replaced] = true
-				delete(request.Headers, header)
-			}
-		}
-	}
-}
-
-func getReplacement(value interface{}) string {
-	switch value.(type) {
-	default:
-		return "<<Error: invalid tpe>>"
-	case bool:
-		return strconv.FormatBool(value.(bool))
-	case int:
-		return strconv.Itoa(value.(int))
-	case string:
-		return value.(string)
-	}
-}
-
-
-func addEndpoint(name string, yaml *simpleyaml.Yaml) {
-	endpoint := &Endpoint{Name: name, Headers: make(map[string]bool)};
-	endpoints[name] = endpoint
-
-	path, err := yaml.GetPath("endpoints", name, "path").String()
-	if err != nil {
-		panic(err)
-	}
-	endpoint.Path = path
-
-	query, err := yaml.GetPath("endpoints", name, "query").String()
-	if err == nil {
-		endpoint.Query = query
-	}
-
-	url, err := yaml.GetPath("endpoints", name, "url").String()
-	if err == nil {
-		endpoint.Url = url
-	} else {
-		endpoint.Url = globalUrl
-	}
-
-	method, err := yaml.GetPath("endpoints", name, "method").String()
-	if err == nil {
-		endpoint.Method = method
-	} else {
-		endpoint.Method = "GET"
-	}
-
-	data, err := yaml.GetPath("endpoints", name, "data").Bool()
-	if err == nil {
-		endpoint.HasData = data
-	}
-
-	headers, err := yaml.GetPath("endpoints", name, "headers").Array()
-	if err == nil {
-		for i := range headers {
-			endpoint.Headers[headers[i].(string)] = true
-		}
-	}
-
-	for globalHeader := range globalHeaders {
-		endpoint.Headers[globalHeader] = true
-	}
-}
 
 func (endpoint Endpoint) show() {
 	t := template.Must(template.New("curlTemplate").Parse(showCurlTemplate))
@@ -309,7 +147,7 @@ func (request Request) run() {
 		}
 	}
 
-	run(requestAsString)
+	executeCurlCommand(requestAsString)
 }
 
 func (endpoint Endpoint) run() {
@@ -331,10 +169,10 @@ func (endpoint Endpoint) run() {
 		}
 	}
 
-	run(endpointAsString)
+	executeCurlCommand(endpointAsString)
 }
 
-func run(command string) {
+func executeCurlCommand(command string) {
 	cmd := exec.Command("curl", strings.Split(command, "\n")...)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
@@ -350,19 +188,4 @@ func run(command string) {
 
 func hasResolvedAllVariables(request string) bool {
 	return strings.Index(request, "{") == -1
-}
-
-func addConfiguration(name string, yaml *simpleyaml.Yaml) {
-	if name == "headers" {
-		headers, _ := yaml.Get(name).Array()
-		for i := range headers {
-			globalHeaders[headers[i].(string)] = true
-		}
-	} else if name == "url" {
-		globalUrl, _ = yaml.Get(name).String()
-	}
-}
-
-func isConfiguration(name string) bool {
-	return name == "headers" || name == "url"
 }
