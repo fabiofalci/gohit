2a2bf9c64e74a0741c7dd90fc0524c2586266d6d
Fabio Falci <fabiofalci@gmail.com>
2017-01-11 23:24:26 +0000

    Add templates code to printer.go

--- printer.go
+++ printer.go
@@ -5,7 +5,37 @@ import (
 	"text/template"
 	"sort"
 	"io"
+	"regexp"
+	"strings"
 )
+
+const showCurlTemplate = `curl '{{.Url}}{{.Path}}{{if .Query}}?{{.Query}}{{end}}' \
+{{- if .Headers}}
+        {{- range $key, $value := .Headers }}
+        -H '{{$key}}' \
+        {{- end}}
+{{- end}}
+{{- if .Options}}
+        {{- range $key, $value := .Options }}
+        {{$key}} \
+        {{- end}}
+{{- end}}
+        -X{{.Method}}
+`
+
+// A separated template for running as it needs to transform the command to an array of string.
+// It splits on newline.
+const runCurlTemplate = `{{.Url}}{{.Path}}{{if .Query}}?{{.Query}}{{end}}
+{{- if .Headers}}
+        {{- range $key, $value := .Headers }}
+-H
+{{$key}}
+        {{- end}}
+{{- end}}
+{{- if .Options}}
+{{.OptionsAsToken}}
+{{- end}}
+-X{{.Method}}`
 
 type Printer struct {
 	conf   *Configuration
@@ -60,3 +90,26 @@ func (printer *Printer) showExecutable(executable Executable) {
 	fmt.Fprintf(printer.writer, "Endpoint %v:\n", executable.GetName())
 	t.Execute(printer.writer, executable)
 }
+
+// methods used by the templates
+
+func (request *Request) OptionsAsToken() string {
+	return executableOptionsAsToken(request)
+}
+
+func (endpoint *Endpoint) OptionsAsToken() string {
+	return executableOptionsAsToken(endpoint)
+}
+
+func executableOptionsAsToken(executable Executable) string {
+	oneLineOptions := ""
+	for option := range executable.GetOptions() {
+		re := regexp.MustCompile("[^\\s\"']+|\"([^\"]*)\"|'([^']*)'")
+		for _, v := range re.FindAllString(option, -1) {
+			oneLineOptions = oneLineOptions + "\n" + v
+		}
+	}
+
+	return strings.TrimPrefix(oneLineOptions, "\n")
+}
+
